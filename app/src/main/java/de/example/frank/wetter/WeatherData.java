d5c5ca4555e77cec38a595445235d14ca3c0ac4e
Administrator <f.neumann.berlin@googlemail.com>
2016-09-19 07:58:23 +0200

    Initial commit

---
+++ app/src/main/java/de/example/frank/wetter/WeatherData.java
@@ -0,0 +1,16 @@
+package de.example.frank.wetter;
+
+public class WeatherData {
+
+	public String name;
+	public String description;
+	public String icon;
+	public Double temp;
+	
+	public WeatherData(String name, String description, String icon, Double temp) {
+		this.name = name;
+		this.description = description;
+		this.icon = icon;
+		this.temp = temp;
+	}
+}
